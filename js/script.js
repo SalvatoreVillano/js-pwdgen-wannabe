6adf052ae424905a6c69116386bbcdb823627ecd
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-10-05 17:34:56 +0200

    Added automatic password generator

--- js/script.js
+++ js/script.js
@@ -43,5 +43,22 @@ while(conditionInt){
         }
     }
 
+    function generatePassword() {
+        var length = 8,
+            charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+    }
+
+
+
 let password = nome+cognome+colore+eta;
-document.getElementById('password').innerHTML = `La tua password generata e':${password}`
+document.getElementById('password').innerHTML = `La tua password generata e':${password}
+<br> Ti sembra troppo facile? Clicca sul bottone!`
+
+function randomizer(){
+    document.getElementById('passwordGenerata').innerHTML = `La tua password generata e':${generatePassword()}`
+}
